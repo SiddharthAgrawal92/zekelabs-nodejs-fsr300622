5ca5bcdbd13307839550f9f72197528811293459
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-12-12 12:20:17 +0530

    Day - 6 MongoDB Atlas

--- MongoDB/Day 5 - Mongo Operations/Notes and Practice.js
+++ MongoDB/Day 5 - Mongo Operations/Notes and Practice.js
@@ -123,8 +123,11 @@ db.orders.aggregate([
     {
         _id: "Pepperoni",
         //1. total = 0 --> total +=quantity;; 0+5 = 5
+        //1. totalCost = 0 --> totalCost += (price*quantity); 0+1060 = 1060
         // total: 5,
+        // totalCost: 1060,
         //2. total = 5 --> total +=quantity;; 5+3 = 8
+        //2. totalCost = 1060 --> totalCost += (price*quantity); 1060+636 = 1696
         total: 8,
     },
     {
@@ -135,3 +138,88 @@ db.orders.aggregate([
         total: 10
     }
 ]
+
+// totalCost += (price*quantity) --> total = total + (price*quantity);
+
+//aggregate total cost
+db.orders.aggregate(
+    [
+        { $match: { size: "medium" } },
+        {
+            $group: {
+                _id: "$name",
+                name: { $first: "$name" },
+                size: { $last: "$size" },
+                count: { $sum: 1 },
+                totalOrderQuantity: { $sum: "$quantity" },
+                totalCost: { $sum: { $multiply: ["$price", "$quantity"] } }
+            }
+        },
+        { $project: { _id: 0 } },
+        { $sort: { _id: 1, totalOrderQuantity: -1, count: 1 } },
+        { $merge: "orders_summary" }
+    ]
+)
+
+// { $out: "orders_summary" }
+
+//1. out -> This will overwrite the returned documents into the collection (and db if you mention)
+// Note: a new collection is created if doesn't exist
+//internally with "out" db clones the existing collection in temporary database created a new collection with new docs
+//  and then copies the name to this new collection and drops the existing one.
+
+//2. merge -> This will simple write/update(find if matched already exist and updates them if not then creates one)
+//into the collection
+
+db.products.insertMany([
+    {
+        _id: 101,
+        name: 'Biscuit',
+        model: 'A101',
+        ingredients: [
+            { name: "flour", quality: "A" },
+            { name: "sugar", quality: "B" },
+        ],
+        flavor: "sweet",
+        size: "medium"
+    },
+    {
+        _id: 102,
+        name: 'Chocolate',
+        model: 'A102',
+        ingredients: [
+            { name: "butter", quality: "C" },
+            { name: "cocoa", quality: "D" }
+        ],
+        flavor: "sweet",
+        size: "large"
+    }
+])
+
+//match element props from array of objects
+//method-1
+db.products.find({ "ingredients.quality": "C" });
+//method-2
+db.products.find({
+    ingredients: { $elemMatch: { quality: "C", name: "flour" } }
+});
+
+//for updating element props in array of objects
+db.products.updateMany(
+    { "ingredients.quality": "C" },
+    { $set: { "ingredients.$[element].quality": "B" } },
+    { arrayFilters: [{ "element.quality": "C" }] }
+);
+
+//MongoDB Cloud Cluster
+//sid1605
+//U0zj4zB4ogfxXbAA
+
+// ReactJS--> http://localhost:3000
+// NodeJS--> http://localhost:8080
+// MongoDB--> mongodb://localhost:27017(localmachine) 
+// MongoDB--> mongodb+srv://sid1605:U0zj4zB4ogfxXbAA@cluster0.tf4ffv5.mongodb.net/test(Cloud Cluster) 
+
+//mongodb+srv://sid1605:QxunT2Uyi2cj9L6Q@cluster0.3o8fgzr.mongodb.net/test
+
+//Mongoose is a NPM package for doing the MongodB operations from the Javascript code
