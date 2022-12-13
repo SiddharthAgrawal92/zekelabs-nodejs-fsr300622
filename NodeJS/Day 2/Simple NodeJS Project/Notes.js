b698f7bc7db06bbd7d5b32da2242735f0e7a3c92
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-12-13 21:35:13 +0530

    Day - 2 Node JS VS Traditional Server & NPM

---
+++ NodeJS/Day 2/Simple NodeJS Project/Notes.js
@@ -0,0 +1,8 @@
+// to install a npm package locally as a dependency
+// --> npm install  <package_name> --save OR npm i  <package_name>  --save
+
+// to install a npm package locally as a dev-dependency(for development only)
+// --> npm install  <package_name> --save-dev OR npm i <package_name> --save-dev
+
+// to install a package globally 
+// --> npm install -g nodemon
---
+++ NodeJS/Day 2/Simple NodeJS Project/index.js
@@ -0,0 +1,6 @@
+
+let myName = "Siddharth Agrawal".split(" ");
+console.log('First Name: ', myName[0]);
+console.log('Last Name: ', myName[1]);
+
+//"nodemon" is npm package--> this will detect any new changes in the js file and re-run the code inside node environment
