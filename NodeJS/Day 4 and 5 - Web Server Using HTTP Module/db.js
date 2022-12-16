cc6a63267537cc57e5b9fa832cdaa8a1370374e6
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-12-16 22:05:00 +0530

    Day 5 - DB Connectivity, HTTP CRUD Operations & URL Parsing

---
+++ NodeJS/Day 4 and 5 - Web Server Using HTTP Module/db.js
@@ -0,0 +1,72 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+const getMongoClient = async () => {
+    const clusterConnectionString = "mongodb+srv://sid1605:U0zj4zB4ogfxXbAA@cluster0.tf4ffv5.mongodb.net/test"
+    return new MongoClient(clusterConnectionString, { monitorCommands: true });
+}
+
+const getUserList = async () => {
+    const dbClient = await getMongoClient().catch(console.error);
+    try {
+        await dbClient.connect().catch(err => console.error(err));
+        //
+        const itemList = await dbClient.db('company').collection('users').find({}).toArray();
+        return { itemList: itemList };
+    } catch (err) {
+        console.error(err);
+        return
+    } finally {
+        await dbClient.close();
+    }
+}
+
+const createUser = async (userObj) => {
+    const dbClient = await getMongoClient().catch(console.error);
+    try {
+        await dbClient.connect().catch(err => console.error(err));
+        //
+        const insertedUser = await dbClient.db('company').collection('users').insertOne(userObj);
+        // insertOne(insertedId) & insertMany(insertedIds)        
+        return insertedUser;
+    } catch (err) {
+        console.error(err);
+        return
+    } finally {
+        await dbClient.close();
+    }
+}
+
+const updateUser = async (userId, userObj) => {
+    const dbClient = await getMongoClient().catch(console.error);
+    try {
+        await dbClient.connect().catch(err => console.error(err));
+        const updatedUser = await dbClient.db('company').collection('users').updateOne({ _id: ObjectId(userId) }, { $set: userObj });
+        return updatedUser;
+    } catch (err) {
+        console.error(err);
+        return
+    } finally {
+        await dbClient.close();
+    }
+}
+
+const deleteUser = async (userId) => {
+    const dbClient = await getMongoClient().catch(console.error);
+    try {
+        await dbClient.connect().catch(err => console.error(err));
+        const updatedUser = await dbClient.db('company').collection('users').deleteOne({ _id: ObjectId(userId) });
+        return updatedUser;
+    } catch (err) {
+        console.error(err);
+        return
+    } finally {
+        await dbClient.close();
+    }
+}
+
+module.exports = {
+    getUserList,
+    createUser,
+    updateUser,
+    deleteUser
+}
--- NodeJS/Day 4 and 5 - Web Server Using HTTP Module/server.js
+++ NodeJS/Day 4 and 5 - Web Server Using HTTP Module/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const routes = require('./routes');
-const HOSTNAME = '127.0.0.1', PORT = 8080;
+const HOSTNAME = '127.0.0.1', PORT = 8000;
 const server = http.createServer(routes);
 server.listen(PORT, HOSTNAME, () => { console.log(`Server is running at: http://${HOSTNAME}:${PORT}`); });
 
